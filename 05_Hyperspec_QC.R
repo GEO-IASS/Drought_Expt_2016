1b68ade77f1a20a8339de120dc3569f2d095c950
malloryb <mallorylbarnes@gmail.com>
2016-09-06 13:33:13 -0700

    Commented out file cleaning and organization

--- 05_Hyperspec_QC.R
+++ 05_Hyperspec_QC.R
@@ -7,13 +7,24 @@
 library(ggplot2)
 library(hyperSpec)
 
+#Set working directory to location fo hyperspectral ASD Files
 getwd()
 setwd(dir = "C:/Users/Mallory/Dropbox/Mallory_Hyperspectral/9_2_2016_hyperspectral/")
+
+#Read in file, used "col.names" argument to rename columns properly. Will need to figure out
+#how to do this with a list of ASCII files
 test <- read.table("ASCII_Reflectance/b2popmlb_A1_Leaf_5-20-201600000.asd.txt", col.names=c("wavelength", "reflectance"))
+
+#Create Column with filename and then delete the first row of data frame
 test$filename <- (test$reflectance[1:1])
 test = test[-1,]
+
+#Change factors to numeric
 as.numeric(levels(test$wavelength))[test$wavelength]
 as.numeric(levels(test$reflectance))[test$reflectance]
+
+
+
 str(test)
 head(test)
 ggplot(test, aes(x = wavelength, y = reflectance, group=1)) + geom_smooth(method=loess)
