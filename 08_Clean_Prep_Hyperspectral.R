2333bdb8bcd2b2f18abe3d1320a2d513d69237fa
malloryb <mallorylbarnes@gmail.com>
2016-09-25 16:10:18 -0700

    It's working!

--- 08_Clean_Prep_Hyperspectral.R
+++ 08_Clean_Prep_Hyperspectral.R
@@ -8,41 +8,39 @@
 #each date for that plant. SO each plant will have one file containing 8 or 9 spectra (which are themselves the 
 #average of 9 spectra)
 
-library(plyr)
+library(dplyr)
 library(readr)
 
 #Step 1 - create file that plots all 9 spectra together and then averages them----------------
 
 getwd()
-setwd("C:/Users/Mal/Dropbox/Mallory_Hyperspectral/9_2_2016_hyperspectral/ASCII_Reflectance/")
+setwd("C:/Users/rsstudent//Dropbox/Mallory_Hyperspectral/9_2_2016_hyperspectral/ASCII_Reflectance/")
 
 #this command creates a list based on the pattern: used http://regexr.com/ to test expression before using
 file_list <- list.files(pattern = '.*A18*......5-24.*')
 datalist <- lapply(file_list, function(x){read.table(file=x)})
-Reduce(function(x,y) {merge(x,y)}, datalist)
 
+ 
+hlist <- list.files(pattern = '.*A18*......5-24.*')
 
-for (file in file_list){
-  dataset <- read.table(file)
-  dat
-  
-  # if the merged dataset doesn't exist, create it
-  if (!exists("dataset")){
-    dataset <- read.table(file, header=TRUE, sep="\t")
-  }
-  
-  # if the merged dataset does exist, append to it
-  if (exists("dataset")){
-    temp_dataset <-read.table(file, header=TRUE, sep="\t")
-    dataset<-rbind(dataset, temp_dataset)
-    rm(temp_dataset)
-  }
-  
+#User Defined Function: Format Hyperspec: 
+
+#Create Column with filename and then delete the first row of data frame
+Format_hyperspec <- function(test){
+        test$filename <- (test$reflectance[1:1])
+        test = test[-1,]
+        
+        #Change factors to numeric
+        test$wavelength <- as.numeric(levels(test$wavelength))[test$wavelength]
+        test$reflectance <-as.numeric(levels(test$reflectance))[test$reflectance]
+        
+        test
 }
 
 
-library(dplyr)
-library(readr)
-df <- list.files(pattern = '.*A18*......5-24.*') %>% 
-  lapply(read_table) %>% 
-  bind_rows 
+df <- lapply(hlist, FUN=read.table, col.names=c("wavelength", "reflectance")) %>% 
+      lapply(FUN=Format_hyperspec) %>%
+        bind_cols
+
+head(df)
+str(df)
