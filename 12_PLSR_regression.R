0acc4c8b78237907b43edb21b650e6f08d1d367e
malloryb <mallorylbarnes@gmail.com>
2017-01-31 13:34:31 -0700

    PLSR and PLSR formatting

--- 12_PLSR_regression.R
+++ 12_PLSR_regression.R
@@ -43,32 +43,81 @@ library(plsropt)
 #' @import signal
 #' @export
 
+
+#Sample dataset: Peach
+#NIR spectra contains the absorbance values in the wavelength range of 700 - 2498 nm (2 nm)   
+# load a sample data
 data(peach)
+str(peach)
+
+# plot the spectra
+head(peach)
 matplot(colnames(peach$NIR), t(peach$NIR), type = "l", lty = 1,
         xlab = "wavelength (nm)", ylab = "log(1/R)",
         main = "NIR spectra of intact peach fruits")
 
-datAll <- read.csv(file = "C:/Users/rsstudent/Dropbox/Drought_Expt_2016/peachNIR.csv", row.names = 1, check.names = F)
+#CSV containing the sample name, the objective variables (Y-variables) and the 
+#explanatory variables (X-variables, ex. spectral data) in this order
+datAll <- read.csv(file = "C:/Users/Mallory/Dropbox/Drought_Expt_2016/peachNIR.csv", row.names = 1, check.names = F)
 
-rownames(datAll)
+# row name of the data set
+# first five column names
 colnames(datAll)[1:5]
-colnames(datAll)[(NCOL(datAll)-4):NCOL(datAll)]
+# last five column names
+colnames(datAll)[(ncol(datAll)-4):ncol(datAll)]
+# extract X-variables
 x <- extdat(datAll, start = 700, end = 2498)
+# reintegrate Y-variables with X-variables
 peach <- data.frame(datAll[, 1:2], NIR = I(as.matrix(x)))
-
+# extract the range of 700 nm - 1098 nm
 peach$NIR <- extdat(peach$NIR, start = 700, end = 1098)
+# Standard normal variate (SNV)
 peach$NIR <- snv(peach$NIR)
+
+# Savitzky-Golay second derivative
+#1st derivative or 2nd derivative to the data set
+#by using the argument m = 0 , m = 1 or m = 2
+# p is the filter order and n is window size
 peach$NIR <- matsgolay(peach$NIR, p=2, n=11, m=2)
+
+# Auto-scaling
 peach$NIR <- scale(peach$NIR, center = TRUE, scale = TRUE)
 
+#Divide data set into training and test set
 datTrain <- peach[1:50, ]
 datTest <- peach[51:74, ]
 
-
+#plsrPlot draws four kinds of plots which are useful for understanding the model.
 result <- plsrPlot(Brix ~ NIR, data = datTrain, testdata = datTest,
                    ncomp = "auto", maxcomp = 10,
                    validation = "CV", segment.type ="interleaved",
                    output = FALSE)
 
+#Find the optimal model
+# reload dat set
+data(peach)
 
-#Need to get data in proper format: 
+# X-variable ranges to be compared
+xrange <- list(c(700, 1098), c(1100, 2498))
+
+#Divide the data set into training and test data set - skip if want to use all samples as training
+datTrain <- peach[1:50, ]
+datTest <- peach[51:74, ]
+
+#function plsrauto . If you want to use all samples as training set, set data = dat and
+#delete testdata = datTest
+result.all <- plsrauto(Brix ~ NIR, data = datTrain,
+                       testdata = datTest, xrange = xrange,
+                       validation = "CV", segment.type ="interleaved")
+
+#The number of segments is changed by the argument of seguments (default value is 10).
+#The number of (consecutive) rows that are replicates of the same object is set by nrep 
+#Replicates will always be kept in the same segment for preventing overfittng.
+#result.all is sorted by the correlation coefficient of validation set R.val (or R.test if test
+                                                                            # set is used)
+#combination of X-variable range 1100-2498 and prerocessing 
+#method SNV + SG2D(wsize-11pt_forder-2) + Auto-scaling shows the highest R.test .
+result.all[1:5,]
+
+# save the result as a CSV file
+write.csv(result.all, file = "plsrauto_result.csv")
---
+++ 13_PLSR_Format.R
@@ -0,0 +1,7 @@
+#Author: Mallory Barnes
+#Date: 1/31/2017
+#Purpose: Format files for use in PLSRopt package
+#Input: Hyperspectral  Reflectances in ASCII format
+#Output: Single data frame in following format: 
+
+# UniqueID Vcmax Jmax Water_Pot  300 301 302 303 etc. etc. (data must be in wide format)
