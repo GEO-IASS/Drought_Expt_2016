67218a39be56d045bde4b651f916ad35bbccf9d8
malloryb <mallorylbarnes@gmail.com>
2017-02-17 13:01:11 -0700

    Trying different training/testing proportions

--- 12_PLS_Models.R
+++ 12_PLS_Models.R
@@ -6,6 +6,7 @@
 library(pls)
 library(devtools)
 library(signal)
+library(dplyr)
 #install_github("uwadaira/plsropt", dependencies = TRUE)
 library(plsropt)
 
@@ -75,10 +76,21 @@ result <- plsrPlot(Brix ~ NIR, data = datTrain, testdata = datTest,
 
 #Ok now for my own data!!!!!!!!!!!!!
 
-poplar_names <- read.csv("C:/Users/rsstudent/Dropbox/Drought_Expt_2016/poplar_allwavelengths.csv")
+poplar_names <- read.csv("C:/Users/Mallory/Dropbox/Drought_Expt_2016/poplar_allwavelengths.csv")
 str(poplar_names)
+poplar_names$date.y <- as.Date(poplar_names$date.y)
+#When subsetting by genotype: 
+poplar_names <- subset(poplar_names, Genotype=="R-270")
+
+#When ordering by water potential (descending)
+#poplar_names <- poplar_names[order(-poplar_names$Water_Pot),] 
+
+#When ordering by date (descending)
+poplar_names <- poplar_names[order(poplar_names$date.y),] 
+poplar_names$date.y
+
 #get rid of 'x' column, Water_Pot, and Genotype for now 
-poplar_names <- poplar_names[ -c(1, 5:7)]
+poplar_names <- poplar_names[ -c(1, 6:7)]
 #change col names (all say 'x' right now(?))
 names(poplar_names) <- gsub("X", "", names(poplar_names))
 str(poplar_names)
@@ -91,27 +103,42 @@ colnames(poplar_all)[1:5]
 #last five column names
 colnames(poplar_all)[(ncol(poplar_all)-4):ncol(poplar_all)]
 #extract x variables (350 nm - 2500 nm)
-x <-extdat(poplar_all, start=700,end=2500)
+x <-extdat(poplar_all, start=350,end=2500)
 poplar<-data.frame(poplar_all[,1:2], NIR=I(as.matrix(x)))
 #extracting range of 700 nm to 1098 nm
-poplar$NIR<-extdat(poplar$NIR,start=700,end=2500)
+poplar$NIR<-extdat(poplar$NIR,start=350,end=2500)
 #preprocessing: standard normal variate
 poplar$NIR <- snv(poplar$NIR)
 #Savitky-Golay second derivative
 poplar$NIR <- matsgolay(poplar$NIR, p=2, n=11, m=2)
 #Auto-scaling
 poplar$NIR <- scale(poplar$NIR, center = TRUE, scale = TRUE)
-#Dvidide data set into training and test set
-datTrain <- poplar[24:56,]
-datTest <- poplar[1:24, ]
+#Divide data set into training and test set
+
+datTrain <- poplar[-c(1:18),]
+datTest <- poplar[19:28,]
+
+
+#Shuffle Rowwise: 
+#poplar_rand <- poplar[sample(nrow(poplar)),]
+
+str(datTrain)
+str(datTest)
+
+#This is for only obs on June 23rd and 24th
+#datTrain <- poplar[-c(25:33),]
+#datTest <- poplar[25:33,]
+
+#This is for only obs on June 23rd and 24th for GT 52-276
+datTrain <- poplar[-c(13:17),]
+datTest <- poplar[13:17,]
 
 result <- plsrPlot(Vcmax ~ NIR, data = datTrain, testdata = datTest,
                    ncomp = "auto", maxcomp = 10,
                    validation = "CV", segment.type ="interleaved",
                    output = FALSE)
-
-
-
-
+result$validation
+result.all <- plsrauto(Vcmax ~ NIR, data = datTrain, testdata = datTest, xrange = list(c(350, 2500)))
+result.all$R.val
 
 
