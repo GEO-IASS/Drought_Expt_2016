3dafe5263bc7c10017d5db933da2126eb0163f9e
malloryb <mallorylbarnes@gmail.com>
2017-02-21 15:53:38 -0700

    Coded in new hyperspectral indices from Yendreck et al. 2016

--- 05_Hyperspec_QC.R
+++ 05_Hyperspec_QC.R
@@ -67,6 +67,19 @@ calc_NDVI = function(w_860, w_690){((w_860-w_690)/(w_860+w_690))}
 calc_NDWI = function(w_860, w_1240){((w_860-w_1240)/(w_860+w_1240))}
 calc_Datt4 = function(w_672, w_550, w_708){(w_672)/(w_550*w_708)}
 calc_Vogelmann2 = function(w_734, w_747, w_715, w_726){(w_734-w_747)-(w_715+w_726)}
+calc_Maccioni=function(w_780, w_710, w_680){(w_780-w_710)/(w_780-w_680)}
+calc_Double_Difference=function(w_749, w_720, w_701, w_672){(w_749-w_720)-(w_701-w_672)}
+calc_Vogelmann1=function(w_740, w_720){(w_740/w_720)}
+calc_mSR705=function(w_750, w_705, w_445){(w_750-w_445)/(w_705-w_445)}
+calc_mNDVI705=function(w_750, w_705, w_445){(w_750-w_705)/(w_750+w_705-2*w_445)}
+calc_SR3=function(w_750, w_550){(w_750/w_550)}
+calc_SR4=function(w_700, w_670){(w_700/w_670)}
+calc_SR1=function(w_750, w_700){(w_750/w_700)}
+calc_Gitelson=function(w_700){(1/w_700)}
+calc_SR2=function(w_752, w_690){(w_752/w_690)}
+calc_SIPI=function(w_800, w_445, w_680){(w_800-w_445)/(w_800-w_680)}
+calc_mNDVI=function(w_800, w_680, w_445){(w_800-w_680)/(w_800+w_680+-2*w_445)}
+calc_mSRCHL=function(w_800, w_445, w_680){(w_800-w_445)/(w_680-w_445)}
 
 #create list of text files (files must be in working directory); 'pattern' is case-sensitive
 setwd(dir = "C:/Users/Mallory/Dropbox/Mallory_Hyperspectral/9_2_2016_hyperspectral/")
@@ -92,6 +105,22 @@ indices$NDVI <- as.numeric(as.character(indices$NDVI))
 indices$NDWI <- as.numeric(as.character(indices$NDWI))
 indices$Datt4<- as.numeric(as.character(indices$Datt4))
 indices$Vogelmann2<- as.numeric(as.character(indices$Vogelmann2))
+indices$Maccioni<- as.numeric(as.character(indices$Maccioni))
+indices$Double_Difference <-as.numeric(as.character(indices$Double_Difference))
+indices$Vogelmann1 <- as.numeric(as.character(indices$Vogelmann1))
+indices$mSR705 <- as.numeric(as.character(indices$mSR705))
+indices$mNDVI705 <- as.numeric(as.character(indices$mNDVI705))
+indices$SR3 <- as.numeric(as.character(indices$SR3))
+indices$SR4 <- as.numeric(as.character(indices$SR4))
+indices$SR1 <- as.numeric(as.character(indices$SR1))
+indices$Gitelson <- as.numeric(as.character(indices$Gitelson))
+indices$SR2 <- as.numeric(as.character(indices$SR2))
+indices$SIPI <- as.numeric(as.character(indices$SIPI))
+indices$mNDVI <- as.numeric(as.character(indices$mNDVI))
+indices$mSRCHL <- as.numeric(as.character(indices$mSRCHL))
+
+
+
 indices$date <- as.Date(indices$date, format="%m-%d-%Y")
 
 write.csv(indices,"C:/Users/rsstudent/Dropbox/Drought_Expt_2016/Processed_Hyperspec_Files_Added_Indices.csv")
@@ -106,26 +135,57 @@ calc_indices <- function(x){
         ID <-  substr(tmp[1,3], 10,12)
         date <- (substr(tmp[1,3], 19,27))
         observation =(substr(tmp[1,3], 31,32))
-        w_531 =tmp[182,2]
-        w_570 = tmp[221,2]
-        w_690 = tmp[341,2]
-        w_860 = tmp[511,2]
-        w_1240 = tmp[891,2]
-        w_672=tmp[323,2]
+        
+        w_445=tmp[96,2]
+        w_531=tmp[182,2]
         w_550=tmp[201,2]
+        w_570=tmp[221,2]
+        w_670=tmp[321,2]
+        w_672=tmp[323,2]
+        w_680=tmp[331,2]
+        w_690=tmp[341,2]        
+        w_700=tmp[351,2]
+        w_701=tmp[352,2]
+        w_705=tmp[356,2]
         w_708=tmp[359,2]
-        w_734=tmp[385,2]
-        w_747=tmp[398,2]
-        w_715=tmp[366,2]
+        w_710=tmp[361,2]
+        w_715=tmp[366,2]        
+        w_720=tmp[371,2]
         w_726=tmp[377,2]
+        w_734=tmp[385,2]        
+        w_740=tmp[391,2]
+        w_747=tmp[398,2]        
+        w_749=tmp[400,2]
+        w_750=tmp[401,2]
+        w_752=tmp[403,2]
+        w_780=tmp[431,2]
+        w_800=tmp[451,2]
+        w_860=tmp[511,2]
+        w_1240=tmp[891,2]
+        
+        
         PRI = calc_PRI(w_531, w_570)
         NDVI = calc_NDVI(w_860, w_690)
         NDWI = calc_NDWI(w_860, w_1240)
         Datt4 = calc_Datt4(w_672, w_550, w_708)
         Vogelmann2 = calc_Vogelmann2(w_734, w_747, w_715, w_726)
-        indices=as.data.frame(cbind(filename, ID, date, observation, PRI, NDVI, NDWI, Datt4, Vogelmann2))
+        Maccioni=calc_Maccioni(w_780, w_710, w_680)
+        Double_Difference=calc_Double_Difference(w_749, w_720, w_701, w_672)
+        Vogelmann1=calc_Vogelmann1(w_740, w_720)
+        mSR705=calc_mSR705(w_750, w_705, w_445)
+        mNDVI705=calc_mNDVI705(w_750, w_705, w_445)
+        SR3=calc_SR3(w_750, w_550)
+        SR4=calc_SR4(w_700, w_670)
+        SR1=calc_SR1(w_750, w_700)
+        Gitelson=calc_Gitelson(w_700)
+        SR2=calc_SR2(w_752, w_690)
+        SIPI=calc_SIPI(w_800, w_445, w_680)
+        mNDVI=calc_mNDVI(w_800, w_680, w_445)
+        mSRCHL=calc_mSRCHL(w_800, w_445, w_680)
+        indices=as.data.frame(cbind(filename, ID, date, observation, PRI, NDVI, NDWI, Datt4, Vogelmann2, Maccioni, Double_Difference, Vogelmann1, mSR705, mNDVI705, SR3, SR4, SR1, Gitelson, SR2, SIPI, mNDVI, mSRCHL))
         return(indices)
 }
+
 
 # define vectors to store results (mean sortable silt values)
 results = data.frame(txtfiles_subset, indices)
